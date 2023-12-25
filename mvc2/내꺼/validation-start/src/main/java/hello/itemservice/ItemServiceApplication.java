0a25ce1f0effabe50a9b6bf2b2d8dea95a8181c3
jckim22 <jckim229@gmail.com>
2023-12-26 01:40:34 +0900

    20231226

---
+++ mvc2/내꺼/validation-start/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,17 @@
+package hello.itemservice;
+
+import hello.itemservice.domain.item.Item;
+import java.util.ArrayList;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApplication.class, args);
+    }
+
+
+
+}
